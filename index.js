3db760742710d2a71c3864916128d05e3206fc93
Madina <madinatussupova0820@gmail.com>
2024-03-19 10:38:40 +0500

    get url from user's input

--- index.js
+++ index.js
@@ -1,26 +1,43 @@
-import inquirer from 'inquirer';
 import qr from 'qr-image';
 import { createWriteStream, writeFile } from 'fs';
+import express from 'express';
 
-inquirer
-	.prompt([
-		{
-			message: "Type in the URL",
-			name: "url",
+const APP = express();
+const PORT = 3000;
+
+APP.use(express.static('public'));
+APP.use(express.json());
+
+APP.listen(PORT, console.log("Listening on port " + PORT));
+
+APP.get('/', (req, res) => {
+	res.status(200).render('index');
+});
+
+APP.post('/data', (req, res) => {
+	const data = req.body;
+	console.log(data);
+	res.json({ message: "Data sent successfully" });
+
+	const URL = data.userUrl;
+	const qr_png = qr.imageSync(URL, { type: 'png' });
+
+	writeFile("qr_img.png", qr_png, (err) => {
+		if (err) {
+			console.error(err);
+			res.status(500).json({ message: "Error saving QR code image" });
+		} else {
+			console.log("The QR code image has been saved!");
+			writeFile("url.txt", URL, (err) => {
+				if (err) {
+					console.error(err);
+					res.status(500).json({ message: "Error saving URL" });
+				} else {
+					console.log("The URL has been saved!");
+					res.json({ message: "Data sent successfully", imgUrl: "qr_image.png"});
+				}
+			});
 		}
-	])
-	.then((answers) => {
-		const URL = answers.url;
-		const qr_png = qr.image(URL, { type: 'png' });
-		qr_png.pipe(createWriteStream('qr_img.png'));
-		
-		writeFile("url.txt", URL, (err) => {
-			if (err) throw err;
-			console.log("The URL has been saved!");
-		})
-	})
-	.catch((error) => {
-		console.log(error);
 	});
-
+});
 
